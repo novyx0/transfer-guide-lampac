e4579f57342dba2bd1c010ca116795d13a67f6a8
novyx0 <debian@debian-BULLSEYE-live-builder-AMD64>
2025-10-16 18:37:02 -0400

    Initial commit: Lampac VPS configuration

--- ua.js
+++
@@ -1,9 +0,0 @@
-(function () {
-'use strict';
-
-   if (!localStorage.getItem('language')) {  
-       localStorage.setItem('language', 'uk');  
-       localStorage.setItem('tmdb_lang', 'uk');  
-   }
-
-})();
