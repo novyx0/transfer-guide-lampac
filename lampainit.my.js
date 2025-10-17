4cbacc23f808b8418af19d0456275a27d84e3809
novyx0 <strrve@gmail.com>
2025-10-18 02:54:54 +0300

    Update lampainit.my.js

--- lampainit.my.js
+++ lampainit.my.js
@@ -1,24 +1,23 @@
 (function() {
   'use strict';
 
-
   // Первоначальная сортировка меню
   if (!localStorage.getItem('menu_sort'))
     localStorage.setItem('menu_sort', '["Головна","Фільми","Серіали","Релізи","Аніме","Вибране","IPTV","Історія","Каталог","Фільтр","Розклад","Торренти","Полуничка"]');
 
   window.lampa_settings = {
-    torrents_use: true,    // торренты включены
+    torrents_use: true,    // Торренты включены
     demo: false,
     read_only: false,
-    socket_use: false,     // CUB socket отключен
+    socket_use: true,      // Включаем сокеты для CUB
     socket_url: undefined,
-    socket_methods: false, // CUB методы отключены
-    account_use: true,     // локальные закладки
-    account_sync: false,   // CUB синхронизация отключена
-    plugins_store: false,  // CUB магазин отключен
+    socket_methods: true,  // Включаем методы CUB
+    account_use: true,     // Включаем аккаунт для закладок и истории
+    account_sync: true,    // Включаем синхронизацию через CUB
+    plugins_store: false,  // Магазин отключен (можно включить при необходимости)
     iptv: false,
-    feed: false,           // CUB лента отключена
-    white_use: false,      // CUB отключен
+    feed: false,
+    white_use: false,
     push_state: false,
     lang_use: true,
     plugins_use: true,
@@ -27,17 +26,16 @@
 
   window.lampa_settings.disable_features = {
     dmca: true,
-    reactions: true,       // CUB реакции отключены
-    discuss: true,         // CUB комментарии отключены
-    ai: true,              // CUB AI отключен
-    install_proxy: true,   // CUB TMDB proxy отключен
-    subscribe: true,       // CUB подписки отключены
+    reactions: true,
+    discuss: true,
+    ai: true,
+    install_proxy: true,
+    subscribe: true,
     blacklist: true,
     persons: true,
     ads: true,
     trailers: false
   };
-
 
   {lampainit-invc}
 
@@ -57,8 +55,8 @@
         Lampa.Storage.set('lampac_unic_id', unic_id);
       }
 
-      // Убрали cubproxy.js, оставили только privateinit.js
-      Lampa.Utils.putScriptAsync(["{localhost}/privateinit.js?account_email=" + encodeURIComponent(Lampa.Storage.get('account_email', '')) + "&uid=" + encodeURIComponent(Lampa.Storage.get('lampac_unic_id', ''))], function() {});
+      // Загрузка privateinit.js с передачей email и уникального id
+      Lampa.Utils.putScriptAsync(["{localhost}/privateinit.js?account_email=" + encodeURIComponent(Lampa.Storage.get('account_email', '')) + "&uid=" + encodeURIComponent(unic_id)], function() {});
 
       if (window.appready) {
         start();
@@ -71,7 +69,7 @@
         });
       }
 
-	  {pirate_store}
+      {pirate_store}
     }
   }, 200);
 
@@ -82,12 +80,12 @@
     if (Lampa.Storage.get('lampac_initiale', 'false')) return;
 
     Lampa.Storage.set('lampac_initiale', 'true');
-    Lampa.Storage.set('source', 'tmdb');                    // Источник TMDB вместо CUB
+    Lampa.Storage.set('source', 'tmdb');
     Lampa.Storage.set('video_quality_default', '2160');
     Lampa.Storage.set('full_btn_priority', '{full_btn_priority_hash}');
-    Lampa.Storage.set('proxy_tmdb', 'true');                // TMDB proxy всегда включен
+    Lampa.Storage.set('proxy_tmdb', 'true');
     Lampa.Storage.set('poster_size', 'w300');
-    Lampa.Storage.set('protocol', 'https');                 // TMDB использует https
+    Lampa.Storage.set('protocol', 'https');
 
     Lampa.Storage.set('parser_use', 'true');
     Lampa.Storage.set('jackett_url', '{jachost}');
@@ -95,9 +93,7 @@
     Lampa.Storage.set('parser_torrent_type', 'jackett');
 
     var plugins = Lampa.Plugins.get();
-
     var plugins_add = [{initiale}];
-
     var plugins_push = [];
 
     plugins_add.forEach(function(plugin) {
@@ -116,31 +112,29 @@
     if (lampainit_invc)
       lampainit_invc.first_initiale();
 
-    // Скрыть CUB-зависимые элементы интерфейса
+    // Не скрываем меню Вибране и Історія, скрываем лишь ненужные разделы
     Lampa.Settings.listener.follow('open', function(e) {
-      // Скрыть меню: Синхронизация, IPTV
-      $(['account', 'iptv'].map(function(c) {
+      $(['iptv'].map(function(c) {
         return '[data-component="' + c + '"]';
       }).join(','), e.body).remove();
     });
 
     Lampa.Listener.follow('full', function(e) {
       if (e.type == 'complite') {
-        e.object.activity.render().find('.button--subscribe').remove(); // подписка на озвучку
-        $('.open--broadcast').remove(); // открыть на другом устройстве
+        e.object.activity.render().find('.button--subscribe').remove(); // удаляем подписку на озвучку
+        $('.open--broadcast').remove(); // удаляем "Открыть на другом устройстве"
       }
     });
 
-    // Скрыть CUB разделы в главном меню
     Lampa.Listener.follow('app', function(e) {
       if (e.type === 'ready') {
-        $("[data-action=feed]").hide();        // лента
-        $("[data-action=myperson]").hide();    // подписки на актеров
-        $("[data-action=subscribes]").hide();  // подписки
+        $("[data-action=feed]").hide();
+        $("[data-action=myperson]").hide();
+        $("[data-action=subscribes]").hide();
         $("[data-action=mytorrents]").hide();
         $("[data-action=about]").hide();
         $("[data-action=console]").hide();
-        $('.head .notice--icon').remove();     // колокольчик уведомлений
+        $('.head .notice--icon').remove();
       }
     });
   }
